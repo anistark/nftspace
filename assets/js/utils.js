b4404fc07f4c4ab149d348723d8a7a5ca07a04d6
Kumar Anirudha <mail@anirudha.dev>
2022-01-09 19:01:14 +0530

    added abi on file path. [wip] read fjson rom file

---
+++ assets/js/utils.js
@@ -0,0 +1,18 @@
+function loadJSON(path, success, error)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                    success(JSON.parse(xhr.responseText));
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
